883b0230888e77b1e6876d63a1c4beb576859b99
durck <poiulkjhmnv50@gmail.com>
2019-12-30 15:46:05 +0300

    debug

--- serverr/app_serverr/utils.py
+++ serverr/app_serverr/utils.py
@@ -4,11 +4,11 @@ def check_id(str, f):
     try:
         l = id_list.objects.in_bulk()
     except:
-        return {"status": False, "text": "доступ запрещён", "flag": f}
+        return {"status": False, "text": "доступ запрещён!", "flag": f}
     for id in l:
         if str(l[id].number) == str:
             return {"status": True}
-    return {"status": False, "text": "доступ запрещён", "flag": f}
+    return {"status": False, "text": "доступ запрещён.", "flag": f}
 
 
 def check_pass(str):
