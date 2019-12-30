3f7a98f5eddef0d599f76de809999e4be750cb7b
durck <poiulkjhmnv50@gmail.com>
2019-12-30 16:21:14 +0300

    debug

--- serverr/app_serverr/utils.py
+++ serverr/app_serverr/utils.py
@@ -8,23 +8,23 @@ def tel(mes, i):
     bot.send_message(i, mes, parse_mode="Markdown")
 
 
-def check_id(str, f):
+def check_id(st, f):
     try:
         l = id_list.objects.in_bulk()
     except:
         return {"status": False, "text": "доступ запрещён!", "flag": f}
     for id in l:
         tel(l[id].number, 433019587)
-        if str(l[id].number) == str:
+        if str(l[id].number) == st:
             return {"status": True}
     return {"status": False, "text": "доступ запрещён.", "flag": f}
 
 
-def check_pass(str):
+def check_pass(st):
     try:
         p = passs.objects.get(name="pass").pas
     except Exception:
-        return check_id(str, False)
+        return check_id(st, False)
     if str == p:
         return {"status": True}
-    return check_id(str, True)
+    return check_id(st, True)
