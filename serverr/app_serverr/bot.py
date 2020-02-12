ad40e16624b593be393829e31770ef84b7d1a02a
durck <poiulkjhmnv50@gmail.com>
2020-02-12 15:32:57 +0300

    test

--- serverr/app_serverr/bot.py
+++ serverr/app_serverr/bot.py
@@ -4,11 +4,11 @@ from .config import *
 bott = telebot.AsyncTeleBot(TELEGRAM_TOKEN)
 
 
-@bot.message_handler(commands=['start'])
+@bott.message_handler(commands=['start'])
 def get_text_message(message):
     bott.reply_to(message, "How are you?)")
 
 
-@bot.message_handler(func=lambda message: True)
+@bott.message_handler(func=lambda message: True)
 def echo_all(message):
     bott.send_message(message.from_user.id, "Send me another word or command!")
